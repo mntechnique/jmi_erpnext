d666896a04e5a1a81da723e4654eea5007cf7f7d
Revant Nandgaonkar <revant.one@gmail.com>
2017-07-01 18:40:06 +0530

    [Fix] Inherit and Extend POS page

--- jmi_erpnext/public/js/pos_page_js.js
+++ jmi_erpnext/public/js/pos_page_js.js
@@ -1,33 +1,14 @@
 // POS page js.
 
-frappe.pages['pos'].on_page_load = function (wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: __('Point of Sale'),
-		single_column: true
-	});
+var jmi = {};
+jmi.super = erpnext.pos.PointOfSale.prototype;
 
-	wrapper.pos = new jmi.pos(wrapper);
-}
-
-jmi = {};
-
-jmi.pos = erpnext.pos.PointOfSale.extend({
+erpnext.pos.PointOfSale = erpnext.pos.PointOfSale.extend({
 	init: function (wrapper) {
-		this.page_len = 20;
-		this.freeze = false;
-		this.page = wrapper.page;
-		this.wrapper = $(wrapper).find('.page-content');
-		this.set_indicator();
-		this.onload();
-		this.make_menu_list();
-		this.bind_events();
-		this.bind_items_event();
-		this.si_docs = this.get_doc_from_localstorage();
-		this.add_truck();
+		jmi.super.init(wrapper);
+		this.add_scanner();
 	},
-	add_truck: function() {
-		console.log(this);
-		this.page.add_action_icon("fa fa-barcode", function(){console.log("trick")});
+	add_scanner: function() {
+		this.page.set_secondary_action("Scanner", function(){console.log("scanner")}, "fa fa-barcode", true);
 	}
 })
