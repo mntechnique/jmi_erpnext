6a896ba4bcac47276e7ab51f964267465cab717b
saurabh_expo <bhosalesaurabh8@gmail.com>
2017-07-08 16:03:15 +0530

    [WIP] Dialog for Barcode

--- jmi_erpnext/public/js/pos_page_js.js
+++ jmi_erpnext/public/js/pos_page_js.js
@@ -9,6 +9,42 @@ erpnext.pos.PointOfSale = erpnext.pos.PointOfSale.extend({
 		this.add_scanner();
 	},
 	add_scanner: function() {
-		this.page.set_secondary_action("Scanner", function(){console.log("scanner")}, "fa fa-barcode", true);
+		this.page.set_secondary_action("Scanner", function(){
+			console.log("scanner")
+			var dialog = new frappe.ui.Dialog({
+				title: __("Add Barcode"),
+				fields: [
+					{fieldtype: "Data", fieldname: "barcode_no", label: __("Barcode Number"), reqd:1},
+					{fieldtype: "HTML", fieldname: "itemslist", label: __("Items List"), readonly:1}
+				]
+			});
+
+			dialog.fields_dict.barcode_no.$input.on("change",function() {
+				i_list = frappe.get_list("Item", fields=["name", "description"], filters = {"barcode":"barcode"})
+				dialog.fields_dict.itemslist.set_input(i_list);	
+			});
+
+			dialog.set_primary_action(__("Save"), function() {
+				var values = dialog.get_values();
+
+			});
+			dialog.show();
+		}, "fa fa-barcode", true);
 	}
 })
+
+
+
+function show_items_list(frm) {
+	// frappe.call({
+	// 	doc: frm.doc,
+	// 	method: "get_service_details",
+	// 	callback: function(r) {
+	// 		$(frm.fields_dict['service_details_html'].wrapper)
+  //          .html("<div class='text-muted text-center' style='padding-top:5%;'>No Records.</div>");
+
+	// 	    $(frm.fields_dict['service_details_html'].wrapper)
+	// 	        .html(frappe.render_template("service_vehicle_details",{"service_vehicle_details":r.message || []}));
+	// 	}
+	// });
+}
