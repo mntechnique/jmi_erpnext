3a928e2f1150fc3432c2a63bcd17ecba2fd29ee7
saurabh_expo <bhosalesaurabh8@gmail.com>
2017-09-14 18:31:58 +0530

    [Mod] CSV file generator: "-" in place of blank item fields in csv

--- jmi_erpnext/item_data_export.py
+++ jmi_erpnext/item_data_export.py
@@ -4,25 +4,38 @@ from frappe import _
 
 @frappe.whitelist()
 def export_item_sheets():
-	fieldnames = ["barcode","item_code","item_name","description","stock_uom"]
+	items = frappe.get_all("Item", fields=["barcode","item_code","item_name","description","stock_uom"])
 
-	items = frappe.get_all("Item", fields=["barcode","item_code","item_name","description","stock_uom"], order_by="barcode DESC")
+	items = sorted(items, key=lambda v:v.get("barcode"), reverse=True)
+	fieldnames = ["barcode","item_code","item_name","description","stock_uom"]
 	fname = os.path.join(frappe.get_site_path(), "public","files", "ProductCodes_barcode.csv")
 	with open(fname, 'wb') as csvfile:
 		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 		for i in items:
-			writer.writerow(i)	
+			row = check_blank_fields(i,fieldnames)
+			writer.writerow(row)
 
-	items = frappe.get_all("Item", fields=["item_code","barcode","item_name","description","stock_uom"], order_by="item_code DESC")
+	items = sorted(items, key=lambda v:v.get("item_code"), reverse=True)
+	fieldnames = ["item_code","barcode","item_name","description","stock_uom"]
 	fname = os.path.join(frappe.get_site_path(), "public","files", "ProductCodes_itemcode.csv")
 	with open(fname, 'wb') as csvfile:
 		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 		for i in items:
-			writer.writerow(i)
+			row = check_blank_fields(i,fieldnames)
+			writer.writerow(row)
 
-	items = frappe.get_all("Item", fields=["description","barcode","item_code","item_name","stock_uom"], order_by="description DESC")
+	items = sorted(items, key=lambda v:v.get("description"), reverse=True)
+	fieldnames = ["description","barcode","item_code","item_name","stock_uom"]
 	fname = os.path.join(frappe.get_site_path(), "public","files", "ProductCodes_description.csv")
 	with open(fname, 'wb') as csvfile:
 		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 		for i in items:
-			writer.writerow(i)
+			row = check_blank_fields(i,fieldnames)
+			writer.writerow(row)
+
+
+def check_blank_fields(i,fieldnames):
+	for fieldname in fieldnames:
+		if (getattr(i, fieldname) == "") or (i.get(fieldname) == None):
+			setattr(i,fieldname,"-")
+	return i
