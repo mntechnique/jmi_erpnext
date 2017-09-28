1d3de38f0e10de50f979364755505a7b9c7b42b5
saurabh_expo <bhosalesaurabh8@gmail.com>
2017-09-28 15:06:34 +0530

    [Fix] Whitelisted methods in api

--- jmi_erpnext/api.py
+++ jmi_erpnext/api.py
@@ -6,8 +6,10 @@ from .item_data_export import export_item_sheets
 def item_on_insert(self, method):
 	export_item_sheets()
 
+@frappe.whitelist()
 def item_on_update(self, method):
 	export_item_sheets()
 
+@frappe.whitelist()
 def item_on_trash(self, method):
 	export_item_sheets()
