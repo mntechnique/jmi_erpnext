caf3c0674cd3c942d62b2659f7d63f8fb0cebbd9
Saurabh <bhosalesaurabh8@gmail.com>
2018-01-17 13:36:43 +0530

    [Fix] Bug fix

--- jmi_erpnext/public/js/pos_page_js.js
+++ jmi_erpnext/public/js/pos_page_js.js
@@ -258,6 +258,7 @@ try {
 					},
 					on_select_change: () => {
 						this.cart.numpad.set_inactive();
+						this.set_form_action();						
 					},
 					get_item_details: (item_code) => {
 						return this.items.get(item_code);
@@ -276,7 +277,7 @@ try {
 				callback: function(r){
 					var customer_info = {
 						"cust_id" : r.message
-					 };
+					};
 					me.wrapper.find("input[data-fieldname ='jmi_customer_id']").val(customer_info.cust_id);
 					// var html = frappe.render_template("jmi_customer_info", {"customer_info": customer_info})
 					// var customer_info = $(".customer-info");
